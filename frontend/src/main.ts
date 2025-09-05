03d437a6874c900b28997fc08081627b371b4d42
Salvador Chamizo <salvadorchamizo@gmail.com>
2025-09-05 13:07:35 +0200

    refactor: Change main to add specific home header
    Now the home adds a special header that is not loaded in other urls. It
    also adds the twinkling stars

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,15 +1,30 @@
 import { router } from "./router";
 import { registerHandlers } from "./pages/register"
 import { loginHandlers } from "./pages/login"
+import { homeText } from "./pages/home"
 
 function render() {
     const app = document.getElementById("app")!;
     app.innerHTML = router(window.location.hash);
+    const header = document.getElementById("header")!;
+    header.className = "";
+    const home = document.getElementById("home")!;
+    home.innerHTML = "";
+    if (location.hash === "" || location.hash === "#/" || location.hash === "#/home")
+    {
+        const home = document.getElementById("home")!;
+        home.innerHTML = homeText();
+        const header = document.getElementById("header")!;
+        header.className = "header-home";
+    }
 
     if (location.hash === "#/register")
         registerHandlers();
     if (location.hash === "#/login")
         loginHandlers();
+    if (location.hash === "" || location.hash === "#/" || location.hash === "#/home") {
+        import("./pages/home").then(mod => mod.handleStars());
+    }
 }
 
 window.addEventListener("hashchange", render);
