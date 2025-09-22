c03a81f4496d02b17981769637e1716e2c702a31
Salvador Chamizo <salvadorchamizo@gmail.com>
2025-09-22 17:36:49 +0200

    feat: Add functions to userRepository
    I have added two new functions to userRepository, one for find users by
    Id, and other to update if an user enables 2fa

--- auth-service/src/repositories/userRepository.ts
+++ auth-service/src/repositories/userRepository.ts
@@ -9,3 +9,13 @@ export function findUser(username: string) {
     const stmt = db.prepare("SELECT * FROM users WHERE username = ?");
     return stmt.get(username);
 }
+
+export function findUserById(userId: number) {
+    const stmt = db.prepare("SELECT * FROM users WHERE id = ?");
+    return stmt.get(userId);
+}
+
+export function updateUser2FA(secret: any, userId: number) {
+    const stmt = db.prepare("UPDATE users SET totp_secret = ?, is_2fa_enabled = 1 WHERE id = ?")
+    stmt.run(secret.base32, userId);
+}
