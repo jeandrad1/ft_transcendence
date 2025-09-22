502ca512c04880dc514e17b52e4e970adb7bdaa6
Salvador Chamizo <salvadorchamizo@gmail.com>
2025-09-22 17:38:21 +0200

    feat: Add two endpoints to authRoutes
    I have added two new endpoints to authRoutes for 2fa authentication,
    /enable-2fa and /verify-2fa

--- auth-service/src/routes/authRoutes.ts
+++ auth-service/src/routes/authRoutes.ts
@@ -1,9 +1,11 @@
 import { FastifyInstance } from "fastify";
-import { registerController, loginController, refreshController, logoutController } from "../controllers/authController";
+import { registerController, loginController, refreshController, logoutController, verify2FAController, enable2FAController } from "../controllers/authController";
 
 export default async function authRoutes(app: FastifyInstance) {
     app.post("/login", loginController);
     app.post("/register", registerController);
     app.post("/refresh", refreshController);
     app.post("/logout", logoutController);
+    app.post("/verify-2fa", verify2FAController);
+    app.post("/enable-2fa", enable2FAController);
 }
