014eb12f4ebc1b52f5fc1ebc0c420ce1836ee5dc
Salvador Chamizo <salvadorchamizo@gmail.com>
2025-10-26 22:18:57 +0100

    feat: Add protect agains unlogged users in remote
    Now, users are redirected to login if they try to create an online
    tournament if they are not logged in. I think I should add more
    protection in the future for other invalid actions.

--- frontend/src/pages/Tournament/tournamentHandles.ts
+++ frontend/src/pages/Tournament/tournamentHandles.ts
@@ -1,4 +1,4 @@
-import { getAccessToken } from "../../state/authState";
+import { getAccessToken, isLoggedIn } from "../../state/authState";
 import { localTournamentPongPage, playTournamentMatch } from "./localTournament";
 import { createRemoteTournament, getRemoteTournaments, loadTournamentPlayers, openTournamentLobby } from "./remoteTournament";
 import { getTournamentLobbyHTML, getTournamentListHtml, getTournamentRemoteModeHtml, getTournamentAliasEightHtml, getTournamentCanvasFourHtml, getTournamentAliasFourHtml, getTournamentPlayersHtml } from "./tournamentTemplate";
@@ -243,6 +243,10 @@ export async function tournamentHandlers() {
     })
 
     onlineButton?.addEventListener("click", () => {
+        if (!isLoggedIn()) {
+            window.location.hash = "#/login";
+            return ;
+        }
         setTournamentContent(getTournamentRemoteModeHtml());
         isOnline = 1;
         tournamentHandlers();
@@ -258,7 +262,6 @@ export async function tournamentHandlers() {
 
     createOnlineTournamentButton?.addEventListener("click", () => {
         setTournamentContent(getTournamentPlayersHtml());
-        tournamentHandlers();
     })
 
     fourPlayerBtn?.addEventListener("click", async () => {
